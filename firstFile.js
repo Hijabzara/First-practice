362dbd226958c0865c556a7312941229ef8438c1
Aqsa <zarahijab424@gmail.com>
2025-08-05 21:18:34 +0500

    first push

---
+++ firstFile.js
@@ -0,0 +1,12 @@
+// this is first line of code 
+// Jab hamne nkoi new cheez start karni ha to pehly ham github accont par repo banaye gay jis me ham code push kary gay 
+//lawaresh code ko repo k sath connect karn lagy hain 
+// Kese huga 
+// 1 open the terminal 
+// 2nd paste the copy commands
+
+// pata kese chaly k connecy hu gai ha 
+// jab side par changings show hune lag jaye to samjo connect hu gai ha 
+
+
+// 1 = > git status ===> changings check karne k liye 
