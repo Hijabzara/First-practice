1412a2d45f314323cd1bde90e9161784f3c44441
Aqsa <zarahijab424@gmail.com>
2025-08-05 21:53:22 +0500

    this is my 3rd push

---
+++ new.js
@@ -0,0 +1,15 @@
+// this is first line of code 
+// Jab hamne nkoi new cheez start karni ha to pehly ham github accont par repo banaye gay jis me ham code push kary gay 
+//lawaresh code ko repo k sath connect karn lagy hain 
+// Kese huga 
+// 1 open the terminal 
+// 2nd paste the copy commands
+
+// pata kese chaly k connecy hu gai ha 
+// jab side par changings show hune lag jaye to samjo connect hu gai ha 
+
+
+// 1 = > git status ===> changings check karne k liye 
+// 2 = > git add . => for add changings,, changins ko  staged changings me move karna 
+// 3 => git commit -m "First push" => staged chanigings ko commit karna
+// 4 => git push origin main => is command se code hamara push hu jaye ga hmari main branch me 
